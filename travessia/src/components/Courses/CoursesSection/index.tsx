2d954581a572499229f153dc7ade4a238a6d279f
luisa-spl <luisa.spinola9@gmail.com>
2022-07-24 19:52:21 -0300

    feat: course section

---
+++ travessia/src/components/Courses/CoursesSection/index.tsx
@@ -0,0 +1,10 @@
+import s from './style.module.scss';
+
+export function CoursesSection() {
+    return (
+        <section className={s.section}>
+            segunda parte
+        </section>
+    )
+   
+}
