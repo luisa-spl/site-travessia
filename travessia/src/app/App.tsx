d812e8323dde083ce1211b1e07036327de414ee0
luisa-spl <luisa.spinola9@gmail.com>
2022-08-02 22:27:51 -0300

    refactor: responsive typography

--- travessia/src/app/App.tsx
+++ travessia/src/app/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../assets/styles/theme';
 import MainRoutes from './routes'
+import { CookiesConsentment } from 'components/CookieModal';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
           <MainRoutes />
+          <CookiesConsentment />
       </BrowserRouter>
     </ChakraProvider>
   );
--- travessia/src/app/routes.tsx
+++ travessia/src/app/routes.tsx
@@ -1,10 +1,12 @@
+import { PrivacyPolicyPage } from '../page/PrivacyPolicy';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../page/Home';
 
 function MainRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />   
+            <Route path="/" element={<Home />} />
+            <Route path="/privacidade" element={<PrivacyPolicyPage />} />   
         </Routes>
     )
 }
