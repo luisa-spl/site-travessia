055ffd38b0d7c659f8d81616a8ae326b78a78770
luisa-spl <luisa.spinola9@gmail.com>
2022-08-02 22:27:12 -0300

    feat: privacy policy page

---
+++ travessia/src/page/PrivacyPolicy/index.tsx
@@ -0,0 +1,139 @@
+import { Header } from '../../components/Header/HeaderMenu';
+import s from './style.module.scss';
+import { Paragraph, Span, Subtitle, Title } from 'components/Typography';
+
+export function PrivacyPolicyPage() {
+    return (
+        <div className={s.container}>
+            <div className={s.menu}>
+                    <Header />
+            </div>
+            <div className={s.card}>
+                <Title>
+                    Política de Privacidade
+                </Title>
+                <Paragraph>
+                    A sua privacidade é importante para nós. É política do Travessia Pilates respeitar a sua privacidade 
+                    em relação a qualquer informação sua que possamos coletar no site Travessia Pilates, e outros sites 
+                    que possuímos e operamos.
+                </Paragraph>
+                <Paragraph>
+                    Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. 
+                    Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos 
+                    coletando e como será usado.
+                </Paragraph>
+                <Paragraph>
+                    Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. 
+                    Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis para evitar perdas e roubos, 
+                    bem como acesso, divulgação, cópia, uso ou modificação não autorizados.
+                </Paragraph>
+                <Paragraph>
+                    Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.
+                </Paragraph>
+                <Paragraph>
+                    O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre 
+                    o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.
+                </Paragraph>
+                <Paragraph>
+                    Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos 
+                    serviços desejados.
+                </Paragraph>
+                <Paragraph>
+                    O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de Aviso de Privacidad e informações 
+                    pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.
+                </Paragraph>
+                <Title>
+                    Política de Cookies
+                </Title>
+                <Subtitle>
+                    O que são cookies
+                </Subtitle>
+                <Paragraph>
+                    Como é prática comum em quase todos os sites profissionais, este site usa cookies, que são pequenos arquivos baixados no seu computador, 
+                    para melhorar sua experiência. Esta página descreve quais informações eles coletam, como as usamos e por que às vezes precisamos armazenar 
+                    esses cookies. Também compartilharemos como você pode impedir que esses cookies sejam armazenados, no entanto, isso pode fazer o downgrade 
+                    ou 'quebrar' certos elementos da funcionalidade do site.
+                </Paragraph>
+                <Subtitle>
+                    Como usamos os cookies?
+                </Subtitle>
+                <Paragraph>
+                    Utilizamos cookies por vários motivos, detalhados abaixo. Infelizmente, na maioria dos casos, não existem opções padrão do setor para desativar 
+                    os cookies sem desativar completamente a funcionalidade e os recursos que eles adicionam a este site. É recomendável que você deixe todos os 
+                    cookies se não tiver certeza se precisa ou não deles, caso sejam usados para fornecer um serviço que você usa.
+                </Paragraph>
+                <Subtitle>
+                    Desativar cookies
+                </Subtitle>
+                <Paragraph>
+                    Você pode impedir a configuração de cookies ajustando as configurações do seu navegador (consulte a Ajuda do navegador para saber como fazer isso). 
+                    Esteja ciente de que a desativação de cookies afetará a funcionalidade deste e de muitos outros sites que você visita. A desativação de cookies 
+                    geralmente resultará na desativação de determinadas funcionalidades e recursos deste site. Portanto, é recomendável que você não desative os cookies.
+                </Paragraph>
+                <Subtitle>
+                    Cookies que definimos
+                </Subtitle>
+                <Span> - Cookies relacionados a pesquisas</Span>
+                <Paragraph>
+                    Periodicamente, oferecemos pesquisas e questionários para fornecer informações interessantes, ferramentas úteis ou para entender nossa base de usuários 
+                    com mais precisão. Essas pesquisas podem usar cookies para lembrar quem já participou numa pesquisa ou para fornecer resultados precisos após a alteração 
+                    das páginas.
+                </Paragraph>
+                <Span> - Cookies de preferências do site</Span>
+                <Paragraph>
+                    Para proporcionar uma ótima experiência neste site, fornecemos a funcionalidade para definir suas preferências de como esse site é executado quando 
+                    você o usa. Para lembrar suas preferências, precisamos definir cookies para que essas informações possam ser chamadas sempre que você interagir com 
+                    uma página for afetada por suas preferências.
+                </Paragraph>
+                <Subtitle>
+                    Cookies de terceiros
+                </Subtitle>
+                <Paragraph>
+                    Em alguns casos especiais, também usamos cookies fornecidos por terceiros confiáveis. A seção a seguir detalha quais cookies de terceiros você pode encontrar 
+                    através deste site.
+                </Paragraph>
+                <Paragraph>
+                    Este site usa o Google Analytics, que é uma das soluções de análise mais difundidas e confiáveis da Web, para nos ajudar a entender como você usa o site e 
+                    como podemos melhorar sua experiência. Esses cookies podem rastrear itens como quanto tempo você gasta no site e as páginas visitadas, para que possamos 
+                    continuar produzindo conteúdo atraente
+                </Paragraph>
+                <Paragraph>
+                    Para mais informações sobre cookies do Google Analytics, consulte a página oficial do Google Analytics.
+                </Paragraph>
+                <Paragraph>
+                    As análises de terceiros são usadas para rastrear e medir o uso deste site, para que possamos continuar produzindo conteúdo atrativo. Esses cookies podem rastrear 
+                    itens como o tempo que você passa no site ou as páginas visitadas, o que nos ajuda a entender como podemos melhorar o site para você.
+                </Paragraph>
+                <Paragraph>
+                    Periodicamente, testamos novos recursos e fazemos alterações subtis na maneira como o site se apresenta. Quando ainda estamos testando novos recursos, esses cookies 
+                    podem ser usados para garantir que você receba uma experiência consistente enquanto estiver no site, enquanto entendemos quais otimizações os nossos usuários mais apreciam.
+                </Paragraph>
+                <Paragraph>
+                    À medida que vendemos produtos, é importante entendermos as estatísticas sobre quantos visitantes de nosso site realmente compram e, portanto, esse é o tipo de dados que 
+                    esses cookies rastrearão. Isso é importante para você, pois significa que podemos fazer previsões de negócios com precisão que nos permitem analizar nossos custos de 
+                    publicidade e produtos para garantir o melhor preço possível.
+                </Paragraph>
+                <Subtitle>
+                    Compromisso do Usuário
+                </Subtitle>
+                <Paragraph>
+                    O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Travessia Pilates oferece no site e com caráter enunciativo, mas não limitativo:
+                </Paragraph>
+                <Paragraph>
+                    A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;
+                </Paragraph>
+                <Paragraph>
+                    B) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Travessia Pilates, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus 
+                    informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.
+                </Paragraph>
+                <Subtitle>
+                    Mais informações
+                </Subtitle>
+                <Paragraph>
+                    Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies 
+                    ativados, caso interaja com um dos recursos que você usa em nosso site.
+                </Paragraph>
+            </div>
+        </div>
+    )
+}
