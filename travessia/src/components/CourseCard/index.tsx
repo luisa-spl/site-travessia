700b0248336c462a096c8c768591d1a3cdf823bb
luisa-spl <luisa.spinola9@gmail.com>
2022-10-01 18:44:10 -0300

    refactor: courseCard component

--- travessia/src/components/CourseCard/index.tsx
+++ travessia/src/components/CourseCard/index.tsx
@@ -2,17 +2,22 @@ import s from './style.module.scss';
 
 interface CourseCardProps {
     cardImage: string;
+    backgroundPosition?: string;
     cardTitle: string;
     cardSubtitle?: string;
     cardDescription: string;
     duration?: string;
 }
 
-export function CourseCard({ cardImage, cardTitle, cardSubtitle, cardDescription, duration }: CourseCardProps) {
+export function CourseCard({ cardImage, cardTitle, cardSubtitle, cardDescription, duration, backgroundPosition }: CourseCardProps) {
     return (
         <div className={s['container-course']}>
-            <div className={s['card-image']}>
-                <img src={cardImage} alt="curso" />
+            <div
+                className={s['card-image']}
+                style={{
+                    backgroundImage: `url(${cardImage})`,
+                    backgroundPosition: `${backgroundPosition}`
+                }}>
             </div>
             <div className={s.description}>
                 <h2 className={s['card-title']}>{cardTitle}</h2>
